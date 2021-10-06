0810e61d94b88c865bd1434b521255cc80e9f602
luisantoniocrag <tricubeart@gmail.com>
2021-10-06 17:28:00 -0500

    bigint

--- app/src/util/metamask.tsx
+++ app/src/util/metamask.tsx
@@ -11,5 +11,5 @@ const web3 = new Web3(provider);
 
 export const getCkbBalance = async (addr: string): Promise<number> => {
   const balance = await web3.eth.getBalance(addr);
-  return parseInt(balance)/ 100000000;
+  return parseInt(balance);
 };
